340f173735a04ddf16e84eef081fdecb11229253
Rodrigo A. Cavalcanti <rodrigocav94@gmail.com>
2024-04-30 12:15:30 -0300

    Initial storyboard setup with custom collection view cell

---
+++ NamesToFaces/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  NamesToFaces
+//
+//  Created by Rodrigo Cavalcanti on 30/04/24.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+
+    @IBOutlet var name: UILabel!
+    
+}
