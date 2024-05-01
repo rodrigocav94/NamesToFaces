f10d2ce3022b9ac105e1a9cf2b272be155e87210
Rodrigo A. Cavalcanti <rodrigocav94@gmail.com>
2024-05-01 17:07:35 -0300

    Added a UIImagePicker and defined what should it do when the user selects an image

--- NamesToFaces/ViewController.swift
+++ NamesToFaces/ViewController.swift
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UICollectionViewController {
+class ViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
     }
 
 
@@ -24,6 +24,32 @@ class ViewController: UICollectionViewController {
             fatalError("Unable to dequeue PersonCell.")
         }
         return cell
+    }
+    
+    @objc func addNewPerson() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true // This dictionary parameter will contain always contain .editedImage because allowsEditing is true. Otherwise, it would contain the key .originalImage
+        picker.delegate = self // Useful for telling us when the user either selected a picture or cancelled the picker.
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        
+        let imageName = UUID().uuidString
+        let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
+        
+        if let jpegData = image.jpegData(compressionQuality: 0.8) { // UIImage has a jpegData() to convert it to a Data object in JPEG image format
+            try? jpegData.write(to: imagePath) // There's a method on Data called write(to:) that, well, writes its data to disk.
+        }
+        
+        dismiss(animated: true)
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        // All apps that are installed have a directory called Documents where you can save private information for the app, and it's also automatically synchronized with iCloud
+        let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask) // The first parameter asks for the documents directory, and its second parameter adds that we want the path to be relative to the user's home directory.  This returns an array that nearly always contains only one thing: the user's documents directory.
+        return path[0]
     }
 }
 
