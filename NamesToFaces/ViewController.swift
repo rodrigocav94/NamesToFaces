1bd3e01c69fdfffd8d1276a2a0a2cae3c1b16318
Rodrigo A. Cavalcanti <rodrigocav94@gmail.com>
2024-05-01 18:15:27 -0300

    Customized the cell, passed parameters to it and added function to run when the user taps on it.

--- NamesToFaces/ViewController.swift
+++ NamesToFaces/ViewController.swift
@@ -18,13 +18,26 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return people.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
             fatalError("Unable to dequeue PersonCell.")
         }
+        
+        let person = people[indexPath.item]
+        
+        cell.name.text = person.name
+        
+        let path = getDocumentsDirectory().appendingPathComponent(person.image)
+        cell.imageView.image = UIImage(contentsOfFile: path.path)
+        
+        cell.imageView.layer.borderColor = UIColor(white: 0, alpha: 0.3).cgColor
+        cell.imageView.layer.borderWidth = 2
+        cell.imageView.layer.cornerRadius = 3
+        cell.layer.cornerRadius = 7
+        
         return cell
     }
     
@@ -56,6 +69,24 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         // All apps that are installed have a directory called Documents where you can save private information for the app, and it's also automatically synchronized with iCloud
         let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask) // The first parameter asks for the documents directory, and its second parameter adds that we want the path to be relative to the user's home directory.  This returns an array that nearly always contains only one thing: the user's documents directory.
         return path[0]
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let person = people[indexPath.item]
+        
+        let ac = UIAlertController(title: "Rename person", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        ac.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak self, weak ac] _ in
+            guard let newName = ac?.textFields?[0].text else { return }
+            person.name = newName
+            
+            self?.collectionView.reloadData()
+        }))
+        
+        present(ac, animated: true)
     }
 }
 
